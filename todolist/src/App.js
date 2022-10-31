fed06b227fa3139b18f58ee3196369326683486c
geunsoo <geunsoo1106@gmail.com>
2022-10-31 10:43:44 +0900

    불필요한 파일들 제거

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,33 +1,9 @@
-import { useState } from "react";
 import "./App.css";
-import TodoList from "./components/TodoList";
-import TodoInsert from "./components/ToDoinsert";
-import TodoListItem from "./components/TodoListItem";
-const [todos, setTodos] = useState([
-  {
-    id: 1,
-    test: "리액트 기초 알아보기",
-    checked: true,
-  },
-  {
-    id: 2,
-    text: "컴포넌트 스타일링 하기",
-    checked: true,
-  },
-  {
-    id: 3,
-    text: "투두리스트 만들기",
-    checked: false,
-  },
-]);
+
 function App() {
   return (
     <>
-      <TodoInsert />
-      <TodoList todos={todos}>
-        <TodoListItem></TodoListItem>
-      </TodoList>
-      ;
+      <h1>일기장</h1>
     </>
   );
 }
